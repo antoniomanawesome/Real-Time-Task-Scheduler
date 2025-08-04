c22cb63194ea77d47cabd19230a7bfc02284bb8a
antoniomanawesome <antoniomanawesome@gmail.com>
2025-08-03 20:22:10 -0400

    setting up gui

--- src/DynamicArray.h
+++ src/DynamicArray.h
@@ -12,18 +12,6 @@
 class DynamicArray{
         
     public:
-    /*
-    struct Task{
-            int ID;             // Task ID
-            int priority;       // Task priority
-            int period;         // Period of task (0 is aperiodic, >0 is periodic)
-            int next_run_time;  // When the task is scheduled to run next
-
-            Task() : ID(0), priority(0), period(0), next_run_time(0) {}     // Default task constructor
-            Task(int ID, int priority, int period, int run_time): ID(ID),   // Parameterized task constructor
-            priority(priority), period(period), next_run_time(run_time)  {}
-        };*/
-       
 
         DynamicArray();                                     // initialize array with a default capacity
         ~DynamicArray();                                    // deallocating heap memory
--- src/Scheduler.h
+++ src/Scheduler.h
@@ -6,6 +6,6 @@
 void Array_Scheduler(DynamicArray& tasks, int sim_end);
 void Heap_Scheduler(std::vector<Task>& tasks, int sim_end);
 void benchmark();
-void optimized_benchmark();
-void Heap_Scheduler_Quiet(MinHeap& pq, int sim_end);
-void Array_Scheduler_Quiet(DynamicArray& tasks, int sim_end);
+// void optimized_benchmark();
+// void Heap_Scheduler_Quiet(MinHeap& pq, int sim_end);
+// void Array_Scheduler_Quiet(DynamicArray& tasks, int sim_end);
