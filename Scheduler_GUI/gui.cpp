01a4e4e97924411b71466226b1cdb21f54817b2e
Bluematt2002 <47576469+Bluematt2002@users.noreply.github.com>
2025-08-04 18:50:13 -0400

    task table

--- Scheduler_GUI/gui.cpp
+++ Scheduler_GUI/gui.cpp
@@ -1,4 +1,4 @@
-#include "gui.h"
+﻿#include "gui.h"
 #include "imgui.h"
 
 namespace gui {
@@ -14,6 +14,21 @@ namespace gui {
 		//After generating random tasks or adding/removing manually, want to display a table with all of the tasks.
 		//This table should have columns for Task ID, Priority, Next Run Time, and Period
 		//Want a button to add a task and randomly generate them (like we did in vscode)
+		const int NUM_TASKS = 10000;
+		const int SIM_END = 5000;
+
+		// ─── 1) Generate one master list of random tasks ───────────────────
+		std::srand(static_cast<unsigned>(std::time(nullptr)));
+		DynamicArray arrayTasks;
+		std::vector<Task> heapTasks;
+		heapTasks.reserve(NUM_TASKS);
+		// temps for user input:
+		static int newID = 0;
+		static int newPriority = 0;
+		static int newPeriod = 0;
+		static int newNextRun = 0;
+
+
 
 		ImGui::Begin("Task Table");
 		ImGui::Text("After generating/adding tasks, displays a table of all tasks before running the scheduler.");
@@ -28,7 +43,33 @@ namespace gui {
 			ImGui::TableSetupColumn("Period");
 			ImGui::TableHeadersRow();
 			ImGui::EndTable();
+			
+
+			for (int i = 0; i < NUM_TASKS; ++i) {
+				Task t{
+					i,                     // ID
+					std::rand() % 5,       // priority in [0..4]
+					std::rand() % SIM_END, // period in [0..SIM_END-1]
+					std::rand() % 10       // next_run_time in [0..9]
+				};
+				arrayTasks.push_back(t);
+				heapTasks.push_back(t);
+			}
+
 		}
+		if (ImGui::Button("Add a task")) {
+
+			Task t1{
+					i,                     // ID
+					std::rand() % 5,       // priority in [0..4]
+					std::rand() % SIM_END, // period in [0..SIM_END-1]
+					std::rand() % 10       // next_run_time in [0..9]
+			};
+			arrayTasks.push_back(t);
+			heapTasks.push_back(t);
+
+		}
+
 		ImGui::End();
 
 		//Scheduler Controls
